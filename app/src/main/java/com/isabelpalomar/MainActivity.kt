fb6ac1b0460f1d3262ed132c622777f62eae4ea6
Isabel Palomar <IPalomar@wsgc.com>
2019-06-17 14:17:49 -0700

    Integration with Weather API

--- app/src/main/java/com/isabelpalomar/MainActivity.kt
+++ app/src/main/java/com/isabelpalomar/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.isabelpalomar
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import com.isabelpalomar.createApplicationScreenMessage
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import io.ktor.client.engine.okhttp.OkHttpConfig
+import io.ktor.client.engine.okhttp.OkHttpEngine
+import kotlinx.coroutines.*
+import kotlin.coroutines.CoroutineContext
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        findViewById<TextView>(R.id.main_text).text = createApplicationScreenMessage()
-
+        val weatherApi = WeatherApi(OkHttpEngine(OkHttpConfig()))
+        launch(Dispatchers.Main) {
+            try {
+                val result = withContext(Dispatchers.IO) { weatherApi.fetchWeather() }
+                Toast.makeText(this@MainActivity, result.toString(), Toast.LENGTH_LONG).show()
+            } catch (e: Exception) {
+                e.printStackTrace()
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
